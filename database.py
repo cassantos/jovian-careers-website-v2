99ea4541e2616dbd47ac3666d63c282ffeb17cb6
Carlos Augusto Santos <49575263+cassantos@users.noreply.github.com>
2023-03-01 00:49:26 +0000

    load jobs from db

---
+++ database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, text
+import os
+
+my_secret = os.environ['DB_CONNECTION_STRING']
+db_connection_string = my_secret
+
+engine = create_engine(db_connection_string,
+              connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+                }
+              })
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+  
+    jobs = []
+    for row in result:
+      jobs.append(row._mapping)
+  return jobs
