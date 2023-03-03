709995cacdfdb7a9e8a0770cb1cbddda81b4c9dd
Carlos Augusto Santos <49575263+cassantos@users.noreply.github.com>
2023-03-03 03:36:31 +0000

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -16,6 +16,21 @@ def list_jobs():
   for job in JOBS:
     jobs_dict.append(dict(job))
   return (jobs_dict)
+
+@app.route("/job/<id>")
+def show_job(id):
+  
+  job = load_job_from_db(id)
+  if not job: #Nenhum linha foi retornada
+    return "Not found", 404
+  else:
+    job_dict = []
+    for job_details in job:
+      job_dict.append(dict(job_details))
+      
+    return render_template("jobpage.html", job=job_details)
+    #return (job_dict)
+    
   
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, text
 import os
+from flask import render_template
 
 my_secret = os.environ['DB_CONNECTION_STRING']
 db_connection_string = my_secret
@@ -19,3 +20,32 @@ def load_jobs_from_db():
     for row in result:
       jobs.append(row._mapping)
   return jobs
+
+query = "select * from jobs where id = " + '10'
+
+with engine.connect() as conn:
+  result = conn.execute(text(query))
+  #job = results.mappings().all()
+  job = []
+  
+  for row in result:
+    job.append(row._mapping)
+    
+  if not job: #Se estiver vazio
+    print(None)
+  else:
+    print(job)
+  
+def load_job_from_db(id):
+  query = "select * from jobs where id = " + id
+
+  with engine.connect() as conn:
+    result = conn.execute(text(query))
+
+  job = []
+  for row in result:
+    job.append(row._mapping)
+  #if not job: #Se estiver vazio
+  #  return 'None'
+  #else:
+    return job
